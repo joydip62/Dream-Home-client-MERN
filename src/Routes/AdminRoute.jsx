585c1e6527c250875bb49b00e04fab497c1e656c
joydip62 <joydipbanik6@gmail.com>
2023-11-25 01:52:55 +0530

    making admin route for admin secure url

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
+import useAuth from "../hooks/useAuth";
+
+const AdminRoute = ({ children }) => {
+    const { isAdminLoading, isAdmin } = useAdmin();
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading || isAdminLoading) {
+    return (
+      <span className="loading loading-infinity w-1/4 block mx-auto"></span>
+    );
+  }
+  if (user && isAdmin) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default AdminRoute;
